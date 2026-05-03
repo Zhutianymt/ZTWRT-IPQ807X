5315b165ee067b9b73167ef128fed5a8d892d499
Zhutianymt <653090793@qq.com>
2026-05-04 07:56:55 +0800

    Add new package updates for quickstart and diskman

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -47,6 +47,17 @@ UPDATE_PACKAGE() {
 # UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf" 这样会把原有的open-app-filter，luci-app-appfilter，oaf相关组件删除，不会出现coremark错误。
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
+# === 你要的插件 ===
+
+# quickstart（含 luci-app-quickstart）
+UPDATE_PACKAGE "quickstart" "sirpdboy/luci-app-quickstart" "main"
+
+# diskman（新版，修复ntfs问题）
+UPDATE_PACKAGE "diskman" "sbwml/luci-app-diskman" "main"
+
+# istorex（注意名字）
+UPDATE_PACKAGE "istorex" "linkease/istore-ui" "main" "pkg"
+
 UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-25.12"
 UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
 UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
